719560231973ff08ce558d047684cea9839dcee5
anhlinhpiketty <156653857+anhlinhpiketty@users.noreply.github.com>
2025-02-16 12:08:04 +0100

    Identification tous les chevauchement
    Jsp comment gérer les chevauchement au dessus de 3, le pas annuel correspond plus pour ces individus (en gros 30)

--- nettoyage.R
+++ nettoyage.R
@@ -23,5 +23,50 @@ trajpro_clean <- trajpro_clean %>% select(-c(p_nlig, p_gan, debproag ,p_gage, de
 trajpro_wide <- trajpro_clean %>%
   pivot_wider(names_from = Années, values_from = p_gactiv)
 
+#Trouver liste de doublon la plus grande
+max_length <- max(sapply(trajpro_wide, function(col) max(sapply(col, length))))
+print(max_length)
+
+#Trouver un exemple de liste 14
+found <- FALSE  # Indicateur pour arrêter la recherche dès qu'on trouve une cellule
+for (col_name in names(trajpro_wide)) {
+  for (i in seq_along(trajpro_wide[[col_name]])) {
+    if (length(trajpro_wide[[col_name]][[i]]) == 14) {
+      print(paste("Ligne:", i, "| Colonne:", col_name))
+      found <- TRUE
+      break
+    }
+  }
+  if (found) break
+}
+
+#Trouver toutes les longueurs de listes
+unique_lengths <- unique(unlist(lapply(trajpro_wide, function(col) lapply(col, length))))
+print(unique_lengths)
+
+#Nombre de fois où apparait une liste de longueur données
+table(unlist(lapply(trajpro_wide, function(col) lapply(col, length))))
+
+#Trouver un exemple de liste 4
+found <- FALSE  # Indicateur pour arrêter la recherche dès qu'on trouve une cellule
+for (col_name in names(trajpro_wide)) {
+  for (i in seq_along(trajpro_wide[[col_name]])) {
+    if (length(trajpro_wide[[col_name]][[i]]) == 4) {
+      print(paste("Ligne:", i, "| Colonne:", col_name))
+      found <- TRUE
+      break
+    }
+  }
+  if (found) break
+}
+
+# Identifier les lignes contenant des listes de taille 4, 5, 6 ou 14 
+rows_to_extract <- apply(trajpro_wide, 1, function(row) {
+  any(sapply(row, function(cell) length(cell) %in% c(4, 5, 6, 14)))
+})
+valeurs_bizarre <- trajpro_wide[rows_to_extract, ]
+print(valeurs_bizarre)
+
+
 
 
